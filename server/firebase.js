aa499b1238101d086287346573afb14ade87c9c2
nomnomfun <188894493+nomnomfun@users.noreply.github.com>
2025-03-23 20:29:55 -0500

    Fixed file path for firebase secret.

--- server/firebase.js
+++ server/firebase.js
@@ -1,7 +1,8 @@
 import admin from "firebase-admin";
 import fs from "fs";
 
-const serviceAccount = JSON.parse(fs.readFileSync("./secrets/firebaseAdminKey.json", "utf8"));
+//const serviceAccount = JSON.parse(fs.readFileSync("./secrets/firebaseAdminKey.json", "utf8"));
+const serviceAccount = JSON.parse(fs.readFileSync("./etc/secrets/firebaseAdminKey.json", "utf8"));
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
