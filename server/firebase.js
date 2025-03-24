9b9c3f829d29fee5e1bf09aa8324447ecd5a042a
nomnomfun <188894493+nomnomfun@users.noreply.github.com>
2025-03-23 20:38:13 -0500

    Removed the ./ from the path to use the absolute path.

--- server/firebase.js
+++ server/firebase.js
@@ -1,8 +1,15 @@
 import admin from "firebase-admin";
 import fs from "fs";
 
-//const serviceAccount = JSON.parse(fs.readFileSync("./secrets/firebaseAdminKey.json", "utf8"));
-const serviceAccount = JSON.parse(fs.readFileSync("./etc/secrets/firebaseAdminKey.json", "utf8"));
+const secretFilePath = "/etc/secrets/firebaseAdminKey.json";
+
+if (!fs.existsSync(secretFilePath)) {
+  console.error("Firebase Admin key file is missing at:", secretFilePath);
+} else {
+  console.log("Firebase Admin key file found.");
+}
+
+const serviceAccount = JSON.parse(fs.readFileSync(secretFilePath, "utf8"));
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
